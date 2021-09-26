bc74371d4bb4ab99618f83c929680db2736b06f7
varun-affinsys <89970283+varun-affinsys@users.noreply.github.com>
2021-09-26 20:23:16 +0530

    Add files via upload

---
+++ mysite-6-User-Registration/main/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Tutorial
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout, authenticate
+# Create your views here.
+
+def homepage(request):
+    #return HttpResponse("Wow this is an <strong>awesome</strong> tutorial")
+    return render(request=request,
+                  template_name='main/home.html',
+                  context={"tutorials": Tutorial.objects.all})
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("main:homepage")
+        else:
+            for msg in form.error_messages:
+                print(form.error_messages[msg])
+                
+    form = UserCreationForm
+    return render(request,
+                  "main/register.html",
+                  context={"form":form})
+    
